a871ee0b74a69e616bd9f4ef5447fc1eb8f12c57
Alex Taylor <alex.taylor@talktalkplc.com>
2017-10-09 12:11:18 +0100

    console log the users from /users

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const app = express()
+var MongoClient = require('mongodb').MongoClient;
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
 app.get('/users', function(req, res){
-    res.send('Welcome to the users page');
+    //res.send('Welcome to the users page');
+    MongoClient.connect("mongodb://localhost:27017/mega_app", function (err, db) {
+        
+        db.collection('users', function (err, collection) {
+            
+             collection.find().toArray(function(err, items) {
+                if(err) throw err;    
+                console.log(items);         
+            });
+            
+        });
+                    
+    });
 })
 
 app.listen(3000, function () {
