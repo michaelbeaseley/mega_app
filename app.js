213a0be174e533399041a5864c6bcd4830827ce9
Alex Taylor <alex.taylor@talktalkplc.com>
2017-10-09 11:48:32 +0100

    add route for /users

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+app.get('/users', function(req, res){
+    res.send('Welcome to the users page');
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
